2e7a23e0ca2fe093ae1aee193c4977c66529d192
RobertAllen <bobbidigi34@gmail.com>
2018-08-24 15:41:17 -0400

    style: update for responsive layout

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYm9iYmlkaWdpIiwiYSI6ImNqbDd6cG5hODBxdW0za3MycDQ5NDJ6N3kifQ.kknp7lyyxDg4uKO3yU3e5g',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
